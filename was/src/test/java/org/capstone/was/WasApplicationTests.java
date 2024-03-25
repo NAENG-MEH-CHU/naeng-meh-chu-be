a3579f8ff94b33f2761fe42db817706e337ca448
JangAJang <janghee5395@gmail.com>
2024-03-25 15:12:58 +0900

    build: 기본 프로젝트 생성

---
+++ was/src/test/java/org/capstone/was/WasApplicationTests.java
@@ -0,0 +1,13 @@
+package org.capstone.was;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WasApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
