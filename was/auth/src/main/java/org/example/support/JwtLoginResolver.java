b5de06f0dc23afa0c64af284db661cd629352f22
JangAJang <janghee5395@gmail.com>
2024-04-07 11:19:36 +0900

    feat: Jwt토큰을 이용한 어노테이션 구현

---
+++ was/auth/src/main/java/org/example/support/JwtLoginResolver.java
@@ -0,0 +1,59 @@
+package org.example.support;
+
+
+import lombok.AllArgsConstructor;
+import org.example.application.JwtAuthService;
+import org.example.domain.entity.Member;
+import org.example.exception.exceptions.BearerTokenNotFoundException;
+import org.springframework.core.MethodParameter;
+import org.springframework.http.HttpHeaders;
+import org.springframework.web.bind.support.WebDataBinderFactory;
+import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.method.support.ModelAndViewContainer;
+
+import java.util.Objects;
+
+@AllArgsConstructor
+public class JwtLoginResolver implements HandlerMethodArgumentResolver {
+
+    private static final String TOKEN_SEPARATOR = " ";
+    private static final int BEARER_INDEX = 0;
+    private static final int PAYLOAD_INDEX = 1;
+    private static final String BEARER = "Bearer";
+
+    private final JwtAuthService jwtAuthService;
+
+    public boolean supportsParameter(final MethodParameter parameter) {
+        return parameter.hasParameterAnnotation(JwtLogin.class);
+    }
+
+    @Override
+    public Member resolveArgument(final MethodParameter parameter,
+                                  final ModelAndViewContainer mavContainer,
+                                  final NativeWebRequest webRequest,
+                                  final WebDataBinderFactory binderFactory) {
+        String authorizationHeader = webRequest.getHeader(HttpHeaders.AUTHORIZATION);
+        validateAuthorization(authorizationHeader);
+
+        return jwtAuthService.findMemberByJwtPayload(getJwtPayload(Objects.requireNonNull(authorizationHeader)));
+    }
+
+    private void validateAuthorization(final String authorizationHeader) {
+        if (!hasAuthorizationHeader(authorizationHeader) || !isBearerToken(authorizationHeader)) {
+            throw new BearerTokenNotFoundException();
+        }
+    }
+
+    private boolean hasAuthorizationHeader(final String authorizationHeader) {
+        return authorizationHeader != null;
+    }
+
+    private boolean isBearerToken(final String authorizationHeader) {
+        return authorizationHeader.split(TOKEN_SEPARATOR)[BEARER_INDEX].equals(BEARER);
+    }
+
+    private String getJwtPayload(final String authorizationHeader) {
+        return authorizationHeader.split(TOKEN_SEPARATOR)[PAYLOAD_INDEX];
+    }
+}
