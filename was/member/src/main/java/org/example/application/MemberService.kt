f3ddd539177bf8e923bc046469f6040cdf5e06c2
JangAJang <janghee5395@gmail.com>
2024-04-12 21:34:17 +0900

    feat: 회원 삭제의 비즈니스 로직 구현

--- was/member/src/main/java/org/example/application/MemberService.kt
+++ was/member/src/main/java/org/example/application/MemberService.kt
@@ -5,6 +5,7 @@ import org.example.domain.entity.Member
 import org.example.domain.enums.Gender
 import org.example.domain.enums.Gender.*
 import org.example.domain.repository.MemberRepository
+import org.example.exception.exceptions.MemberNotFoundException
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 
@@ -22,6 +23,12 @@ open class MemberService(private val memberRepository: MemberRepository) {
     open fun updateGender(genderString: String, member: Member) {
         member.updateGender(findGenderByInput(genderString));
         memberRepository.save(member);
+    }
+
+    @Transactional
+    open fun deleteMember(member: Member) {
+        memberRepository.findById(member.id).orElseThrow{MemberNotFoundException()}
+        memberRepository.delete(member)
     }
 
     private fun findGenderByInput(genderString: String): Gender {
