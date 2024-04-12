02163662b3a75bc16b9c4c2e52a5f68c0cbbf3f9
JangAJang <janghee5395@gmail.com>
2024-04-12 17:55:12 +0900

    feat: 회원의 성별, 이름을 수정하는 것을 검증하는 단위테스트 구현

---
+++ was/member/src/test/java/application/MemberServiceUnitTest.kt
@@ -0,0 +1,59 @@
+package application
+
+import org.example.application.MemberService
+import org.example.domain.entity.Member
+import org.example.domain.enums.Gender
+import org.example.domain.repository.MemberRepository
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
+import org.mockito.InjectMocks
+import org.mockito.Mock
+import org.mockito.Mockito
+import org.mockito.junit.jupiter.MockitoExtension
+
+@ExtendWith(MockitoExtension::class)
+class MemberServiceUnitTest {
+
+    @Mock
+    private lateinit var memberRepository: MemberRepository
+
+    @InjectMocks
+    private lateinit var memberService: MemberService
+
+    private var member = Member.builder()
+            .nickname("before")
+            .age(10)
+            .gender(Gender.MALE)
+            .email("test@test.com")
+            .ingredients("0")
+            .build();
+
+    @DisplayName("회원의 닉네임을 성공적으로 수정한다.")
+    @Test
+    fun updateNickname() {
+        // Given
+
+        // When
+        Mockito.`when`(memberRepository.save(member)).thenReturn(member)
+        val result = memberService.updateNickname("after", member)
+
+        // Then
+        Assertions.assertEquals(result, Unit)
+    }
+
+    @DisplayName("회원의 성별을 성공적으로 수정한다.")
+    @Test
+    fun updateGender() {
+        // Given
+
+        // When
+        Mockito.`when`(memberRepository.save(member)).thenReturn(member)
+        val result = memberService.updateGender("남성", member)
+
+        // Then
+        Assertions.assertEquals(result, Unit)
+    }
+
+}
