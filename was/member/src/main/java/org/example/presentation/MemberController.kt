2b0048f086e41071716231669f5fb72b4bcbb62d
JangAJang <janghee5395@gmail.com>
2024-04-16 13:12:29 +0900

    feat: 테스트를 위해 빈 컨트롤러 함수 구현

--- was/member/src/main/java/org/example/presentation/MemberController.kt
+++ was/member/src/main/java/org/example/presentation/MemberController.kt
@@ -4,57 +4,81 @@ import jakarta.validation.Valid
 import lombok.RequiredArgsConstructor
 import org.example.application.MemberService
 import org.example.domain.entity.Member
+import org.example.presentation.dto.request.AddUsingReasonRequest
 import org.example.presentation.dto.request.ChangeAgeRequest
 import org.example.presentation.dto.request.ChangeGenderRequest
 import org.example.presentation.dto.request.ChangeNicknameRequest
 import org.example.presentation.dto.response.AgeListResponse
 import org.example.presentation.dto.response.MemberResponse
+import org.example.presentation.dto.response.UsingReasonsResponse
 import org.example.support.JwtLogin
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PatchMapping
+import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/api/member")
 @RequiredArgsConstructor
-open class MemberController (private val memberService: MemberService){
+class MemberController (private val memberService: MemberService){
 
     @GetMapping("/age")
-    open fun findAgeOptions(@JwtLogin member: Member): ResponseEntity<AgeListResponse> {
+    fun findAgeOptions(@JwtLogin member: Member): ResponseEntity<AgeListResponse> {
         val response = AgeListResponse(memberService.findAgeOptions())
         return ResponseEntity<AgeListResponse>(response, HttpStatus.OK)
     }
 
+    @GetMapping("/reasons")
+    fun findAllUsingReasons(@JwtLogin member: Member): ResponseEntity<UsingReasonsResponse> {
+        return ResponseEntity<UsingReasonsResponse>(UsingReasonsResponse(listOf()), HttpStatus.OK)
+    }
+
+    @PostMapping("/reasons")
+    fun addUsingReasons(@JwtLogin member: Member, @RequestBody @Valid addUsingReasonRequest: AddUsingReasonRequest): ResponseEntity<Unit> {
+        return ResponseEntity<Unit>(HttpStatus.CREATED)
+    }
+
+    @GetMapping("/reasons/me")
+    fun findMyUsingReasons(@JwtLogin member: Member): ResponseEntity<UsingReasonsResponse> {
+        return ResponseEntity<UsingReasonsResponse>(UsingReasonsResponse(listOf()), HttpStatus.OK)
+    }
+
+    @DeleteMapping("/reasons")
+    fun deleteMemberReason(@JwtLogin member: Member, @RequestParam(name = "id") id: String): ResponseEntity<Unit> {
+        return ResponseEntity<Unit>(HttpStatus.NO_CONTENT)
+    }
+
     @PatchMapping("/age")
-    open fun updateAge(@JwtLogin member: Member, @RequestBody @Valid request: ChangeAgeRequest): ResponseEntity<Unit> {
+    fun updateAge(@JwtLogin member: Member, @RequestBody @Valid request: ChangeAgeRequest): ResponseEntity<Unit> {
         memberService.updateAge(request, member)
         return ResponseEntity(HttpStatus.OK)
     }
 
     @GetMapping("/me")
-    open fun findMemberData(@JwtLogin member: Member): ResponseEntity<MemberResponse> {
+    fun findMemberData(@JwtLogin member: Member): ResponseEntity<MemberResponse> {
         return ResponseEntity<MemberResponse>(MemberResponse(member), HttpStatus.OK)
     }
 
     @PatchMapping("/nickname")
-    open fun updateNickname(@JwtLogin member: Member, @RequestBody @Valid request: ChangeNicknameRequest): ResponseEntity<Unit> {
+    fun updateNickname(@JwtLogin member: Member, @RequestBody @Valid request: ChangeNicknameRequest): ResponseEntity<Unit> {
         memberService.updateNickname(request.getNickname(), member)
         return ResponseEntity(HttpStatus.OK)
     }
 
     @PatchMapping("/gender")
-    open fun updateGender(@JwtLogin member: Member, @RequestBody @Valid request: ChangeGenderRequest): ResponseEntity<Unit> {
+    fun updateGender(@JwtLogin member: Member, @RequestBody @Valid request: ChangeGenderRequest): ResponseEntity<Unit> {
         memberService.updateGender(request.getGender(), member)
         return ResponseEntity(HttpStatus.OK)
     }
 
     @DeleteMapping("")
-    open fun deleteMember(@JwtLogin member: Member): ResponseEntity<Unit> {
+    fun deleteMember(@JwtLogin member: Member): ResponseEntity<Unit> {
         memberService.deleteMember(member)
         return ResponseEntity(HttpStatus.NO_CONTENT)
     }
