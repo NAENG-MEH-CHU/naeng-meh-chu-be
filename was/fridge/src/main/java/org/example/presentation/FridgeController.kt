9e5695a6760d3469132cbae77b10096d60227e4f
JangAJang <janghee5395@gmail.com>
2024-04-18 21:09:04 +0900

    feat: 단위테스트를 통과하는 컨트롤러 로직 구현

--- was/fridge/src/main/java/org/example/presentation/FridgeController.kt
+++ was/fridge/src/main/java/org/example/presentation/FridgeController.kt
@@ -4,9 +4,11 @@ import jakarta.validation.Valid
 import org.example.application.FridgeService
 import org.example.domain.entity.Member
 import org.example.presentation.dto.request.AddIngredientRequest
+import org.example.presentation.dto.response.IngredientsResponse
 import org.example.support.JwtLogin
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
@@ -20,5 +22,10 @@ open class FridgeController( private val fridgeService: FridgeService ) {
     open fun addIngredient(@JwtLogin member: Member, @RequestBody @Valid request: AddIngredientRequest): ResponseEntity<Unit> {
         fridgeService.addIngredient(request, member)
         return ResponseEntity(HttpStatus.CREATED)
+    }
+
+    @GetMapping("")
+    fun findAllIngredients(@JwtLogin member: Member): ResponseEntity<IngredientsResponse> {
+        return ResponseEntity(fridgeService.findAllIngredients(), HttpStatus.OK)
     }
 }
