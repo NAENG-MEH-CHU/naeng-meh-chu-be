086bc0dc24e4f143e73d60ad44fb3f069fd72a1d
JangAJang <janghee5395@gmail.com>
2024-04-18 17:04:28 +0900

    test: 전체 재료를 조회하는 컨트롤러 단위테스트 구현

--- was/fridge/src/test/java/presentation/FridgeControllerUnitTest.kt
+++ was/fridge/src/test/java/presentation/FridgeControllerUnitTest.kt
@@ -8,6 +8,7 @@ import org.example.domain.enums.Gender
 import org.example.exception.exceptions.IngredientNotFoundException
 import org.example.presentation.FridgeController
 import org.example.presentation.dto.request.AddIngredientRequest
+import org.example.presentation.dto.response.IngredientsResponse
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
@@ -60,5 +61,19 @@ class FridgeControllerUnitTest {
 
         //then
         shouldThrow<IngredientNotFoundException> { fridgeController.addIngredient(member, request) }
+    }
+
+    @DisplayName("전체 재료 조회를 성공한다.")
+    @Test
+    fun findAllIngredients_success() {
+        // given
+        val ingredients = IngredientsResponse(listOf())
+
+        // when
+        Mockito.`when`(fridgeService.findAllIngredients())
+            .thenReturn(ingredients)
+
+        // then
+        fridgeController.findAllIngredients(member) shouldBe ResponseEntity<IngredientsResponse>(IngredientsResponse(listOf()), HttpStatus.OK)
     }
 }
