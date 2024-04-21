738f97c47fd2b85d8784642684944788b2ba3b70
JangAJang <janghee5395@gmail.com>
2024-04-21 17:53:09 +0900

    test: 나의 재료 조회 비즈니스 로직 통합테스트 구현

--- was/fridge/src/test/java/application/FridgeServiceIntegrationTest.kt
+++ was/fridge/src/test/java/application/FridgeServiceIntegrationTest.kt
@@ -103,13 +103,17 @@ class FridgeServiceIntegrationTest(
     }
 
     @DisplayName("나의 재료 조회를 성공한다")
+    @Test
     fun findMyIngredients_success() {
         // given
-
+        val request = AddIngredientRequest(ingredient.id, 2017, 3, 1)
+        fridgeService.addIngredient(request, member)
 
         // when
+        val result = fridgeService.findMyIngredients(member)
 
         // then
+        result.myIngredients.size shouldBe 1
     }
 
     @AfterEach
