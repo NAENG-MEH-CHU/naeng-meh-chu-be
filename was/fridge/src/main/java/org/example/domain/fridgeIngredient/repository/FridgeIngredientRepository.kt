9143ed00af5559508082492008bf4e7f59743b24
JangAJang <janghee5395@gmail.com>
2024-04-25 13:47:15 +0900

    feat: Query를 이용해 원하는 마감일 기준으로 유통기한 임박 재료를 조회하는 로직 구현'

--- was/fridge/src/main/java/org/example/domain/fridgeIngredient/repository/FridgeIngredientRepository.kt
+++ was/fridge/src/main/java/org/example/domain/fridgeIngredient/repository/FridgeIngredientRepository.kt
@@ -2,6 +2,8 @@ package org.example.domain.fridgeIngredient.repository
 
 import org.example.domain.fridgeIngredient.entity.FridgeIngredient
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
+import java.time.LocalDate
 import java.util.UUID
 
 interface FridgeIngredientRepository: JpaRepository<FridgeIngredient, UUID> {
@@ -9,4 +11,7 @@ interface FridgeIngredientRepository: JpaRepository<FridgeIngredient, UUID> {
     fun existsByIngredientIdAndMemberId(ingredientId: Int, memberId: UUID): Boolean
 
     fun findAllByMemberId(memberId: UUID): List<FridgeIngredient>
+
+    @Query("select f from FridgeIngredient f where f.expiresAt <= ?1 order by f.expiresAt ASC")
+    fun findFridgeIngredientsExpiresWithin(endDate: LocalDate): List<FridgeIngredient>
 }
