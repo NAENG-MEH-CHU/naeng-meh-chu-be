e42e6216474103050cd03d0c39c879b02e9854cf
JangAJang <janghee5395@gmail.com>
2024-04-29 13:07:45 +0900

    fix: Redis Pub/Sub의 ip와 포트, 비밀번호 등록을 위해 @Value로 데이터를 사용하도록 수정

--- was/auth/src/main/java/org/example/config/RedisConfig.java
+++ was/auth/src/main/java/org/example/config/RedisConfig.java
@@ -4,9 +4,12 @@ import org.example.application.MemberEventHandler;
 import org.example.domain.event.AddIngredientEvent;
 import org.example.domain.event.RemoveIngredientEvent;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisPassword;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.listener.ChannelTopic;
@@ -18,9 +21,22 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 @Configuration
 public class RedisConfig {
 
+    @Value("${spring.data.redis.host}")
+    private String host;
+
+    @Value("${spring.data.redis.port}")
+    private int port;
+
+    @Value("${spring.data.redis.password}")
+    private String password;
+
     @Bean
     public RedisConnectionFactory redisConnectionFactory() {
-        return new LettuceConnectionFactory();
+        RedisStandaloneConfiguration configuration = new RedisStandaloneConfiguration();
+        configuration.setHostName(host);
+        configuration.setPort(port);
+        configuration.setPassword(RedisPassword.of(password));
+        return new LettuceConnectionFactory(configuration);
     }
 
     @Bean
