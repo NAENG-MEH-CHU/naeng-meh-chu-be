ed81a44e2c22ba6392aa72be82777aa10ce25c70
JangAJang <janghee5395@gmail.com>
2024-05-01 13:44:18 +0900

    test: 컨트롤러 통합테스트 구현(단위에서는 예외가 발생할 경우가 없음)

--- was/recipe/src/test/java/presentation/RecipeControllerIntegrationTest.kt
+++ was/recipe/src/test/java/presentation/RecipeControllerIntegrationTest.kt
@@ -76,7 +76,7 @@ class RecipeControllerIntegrationTest(
             .age(Age.TWENTIES)
             .gender(Gender.MALE)
             .email("test@test.com")
-            .ingredients(0)
+            .ingredients(2)
             .build()
         memberRepository.save(member)
         accessToken = jwtTokenProvider.createAccessToken(member.id.toString())
@@ -133,6 +133,44 @@ class RecipeControllerIntegrationTest(
                 requestHeaders(
                     headerWithName(HttpHeaders.AUTHORIZATION).description("로그인 후 제공되는 Bearer 토큰")
                 ),
+            ))
+            .andReturn()
+    }
+
+    @DisplayName("내 재료로 만들 수 있는 레시피를 조회한다")
+    @Test
+    fun `내 재료로 만들 수 있는 레시피를 조회한다`() {
+        // given
+        for(index: Int in 1..100) {
+            recipeRepository.save(Recipe(index.toLong(), "tester${index}", "link${index}", "thumbnail${index}"))
+        }
+        // expected
+        mockMvc.perform(
+            RestDocumentationRequestBuilders.get("/api/recipe/ingredients")
+                .header(HttpHeaders.AUTHORIZATION, "Bearer $accessToken"))
+            .andExpect(MockMvcResultMatchers.status().isOk)
+            .andDo(customDocument(
+                "find_by_members_ingredients",
+                requestHeaders(
+                    headerWithName(HttpHeaders.AUTHORIZATION).description("로그인 후 제공되는 Bearer 토큰")
+                ),
+            ))
+            .andReturn()
+    }
+
+    @DisplayName("토큰이 없을 때 내 재료로 만들 수 있는 레시피 조회를 실패한다")
+    @Test
+    fun `토큰이 없을 때 내 재료로 만들 수 있는 레시피 조회를 실패한다`() {
+        // given
+        for(index: Int in 1..100) {
+            recipeRepository.save(Recipe(index.toLong(), "tester${index}", "link${index}", "thumbnail${index}"))
+        }
+        // expected
+        mockMvc.perform(
+            RestDocumentationRequestBuilders.get("/api/recipe/ingredients"))
+            .andExpect(MockMvcResultMatchers.status().isUnauthorized)
+            .andDo(customDocument(
+                createFailedIdentifier("find_by_members_ingredients", NO_TOKEN),
             ))
             .andReturn()
     }
