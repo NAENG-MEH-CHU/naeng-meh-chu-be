bd1e43702b3e1e0105d08d0061bbf6ab39a95391
JangAJang <janghee5395@gmail.com>
2024-05-17 16:47:01 +0900

    feat: Auth 모듈에서 회원의 이용내역 추가 로직을 수행하도록 구현

--- was/auth/src/main/java/org/example/domain/enums/UsingReason.java
+++ was/auth/src/main/java/org/example/domain/enums/UsingReason.java
@@ -1,5 +1,11 @@
 package org.example.domain.enums;
 
+import lombok.Getter;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Getter
 public enum UsingReason {
     DIET("다이어트를 하는데 집에 있는 음식들로 식단을 관리하고 싶어요."),
     VARIETY("좀 더 다양한 음식을 해 먹고 싶어요."),
@@ -7,12 +13,19 @@ public enum UsingReason {
     HEALTH("몸에 좋은 건강한 레시피를 추천받고 싶어요.");
 
     private final String content;
+    private static final Map<String, UsingReason> CONTENT_TO_REASON_MAP = new HashMap<>();
+
+    static {
+        for (UsingReason reason : UsingReason.values()) {
+            CONTENT_TO_REASON_MAP.put(reason.getContent(), reason);
+        }
+    }
 
     UsingReason(String content) {
         this.content = content;
     }
 
-    public String getContent() {
-        return content;
+    public static UsingReason parseReason(final String content) {
+        return CONTENT_TO_REASON_MAP.get(content);
     }
 }
