513ad93b6de84e3d6585afe9e0a331ea1168e0fe
JangAJang <janghee5395@gmail.com>
2024-06-05 13:42:52 +0900

    feat: 군집화를 위한 객체들 구현

--- was/recipe/src/main/java/org/example/infrastructure/KMeansClusterer.kt
+++ was/recipe/src/main/java/org/example/infrastructure/KMeansClusterer.kt
@@ -1,31 +1,31 @@
-package org.example.infrastructure
-
-import org.apache.commons.math3.ml.clustering.*
+import org.apache.commons.math3.ml.clustering.CentroidCluster
+import org.apache.commons.math3.ml.clustering.KMeansPlusPlusClusterer
 import org.apache.commons.math3.ml.distance.EuclideanDistance
 import org.example.domain.memberRecipe.entity.MemberRecipe
-import org.springframework.stereotype.Service
+import org.example.infrastructure.DataProcessor
+import org.example.infrastructure.DoubleArrayWrapper
 import java.util.*
 
-
-@Service
 class KMeansClusterer(
+    private val dataProcessor: DataProcessor
 ) {
-    fun clusterData(data: List<DoubleArray>?, numClusters: Int): List<CentroidCluster<DoubleArray>> {
-        val clusterer: KMeansPlusPlusClusterer<DoubleArray> =
-            KMeansPlusPlusClusterer(numClusters, 1000, EuclideanDistance())
-        return clusterer.cluster(data)
+
+    fun clusterData(data: List<DoubleArray>?, numClusters: Int): List<CentroidCluster<DoubleArrayWrapper>> {
+        val clusterableData = data?.map { DoubleArrayWrapper(it) } ?: emptyList()
+        val clusterer = KMeansPlusPlusClusterer<DoubleArrayWrapper>(numClusters, 1000, EuclideanDistance())
+        return clusterer.cluster(clusterableData)
     }
 
     fun findClusterIndexForRecipe(
         recipeId: UUID,
-        clusters: List<CentroidCluster<DoubleArray>>,
+        clusters: List<CentroidCluster<DoubleArrayWrapper>>,
         recipes: List<MemberRecipe>
     ): Int {
         for (i in clusters.indices) {
-            for (point in clusters[i].getPoints()) {
+            for (point in clusters[i].points) {
                 val pointArray = point.point
                 for (recipe in recipes) {
-                    val recipeArray = doubleArrayOf(ageToDouble(recipe.memberAge), genderToDouble(recipe.gender))
+                    val recipeArray = dataProcessor.transformData(recipe)
                     if (Arrays.equals(pointArray, recipeArray) && recipe.recipeId == recipeId) {
                         return i
                     }
@@ -37,24 +37,25 @@ class KMeansClusterer(
 
     fun getRecipesInSameCluster(
         recipeId: UUID,
-        clusters: List<CentroidCluster<DoubleArray>>,
+        clusters: List<CentroidCluster<DoubleArrayWrapper>>,
         recipes: List<MemberRecipe>
     ): List<MemberRecipe> {
         val clusterIndex = findClusterIndexForRecipe(recipeId, clusters, recipes)
         if (clusterIndex == -1) {
-            return Collections.emptyList()
+            return emptyList()
         }
 
         val sameClusterRecipes: MutableList<MemberRecipe> = ArrayList()
-        for (point in clusters[clusterIndex].getPoints()) {
+        for (point in clusters[clusterIndex].points) {
             val pointArray = point.point
             for (recipe in recipes) {
-                val recipeArray = doubleArrayOf(ageToDouble(recipe.memberAge), genderToDouble(recipe.gender))
+                val recipeArray = dataProcessor.transformData(recipe)
                 if (Arrays.equals(pointArray, recipeArray)) {
                     sameClusterRecipes.add(recipe)
                 }
             }
         }
+
         return sameClusterRecipes
     }
 }
