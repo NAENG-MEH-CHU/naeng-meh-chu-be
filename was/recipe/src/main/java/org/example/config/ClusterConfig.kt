441f3d6722ab1135106ec1dcd4ec7ba360e82013
JangAJang <janghee5395@gmail.com>
2024-06-05 19:36:14 +0900

    fix: Controller dto의 val 누락 수정

--- was/recipe/src/main/java/org/example/config/ClusterConfig.kt
+++ was/recipe/src/main/java/org/example/config/ClusterConfig.kt
@@ -1,12 +1,15 @@
 package org.example.config
 
 import KMeansClusterer
+import org.example.domain.cluster.repository.ClusterRecipeRepository
 import org.example.infrastructure.DataProcessor
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
 @Configuration
-open class ClusterConfig {
+open class ClusterConfig(
+    private val clusterRecipeRepository: ClusterRecipeRepository
+) {
 
     @Bean
     open fun dataProcessor(): DataProcessor {
@@ -15,6 +18,6 @@ open class ClusterConfig {
 
     @Bean
     open fun cluster(): KMeansClusterer {
-        return KMeansClusterer(dataProcessor())
+        return KMeansClusterer(dataProcessor(), clusterRecipeRepository)
     }
 }
